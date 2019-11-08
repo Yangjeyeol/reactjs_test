93374212eb0eb8a40fef07792c4163d972474099
yang10 <yangjeyeol@gmail.com>
2019-11-08 18:08:13 +0900

    redux study todos

--- redux-test/src/containers/TodosContainer.js
+++ redux-test/src/containers/TodosContainer.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 import Todos from 'components/Todos';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'
+import * as todoActions from 'store/modules/todo';
 
 class TodosContainer extends Component {
+    handleChange = () => {
+
+    }
+
+    handleInsert = () => {
+
+    }
+
+    handleToggle = () => {
+
+    }
+
+    handleRemove = () => {
+
+    }
+
     render() {
         return (
             <Todos />
@@ -9,4 +28,12 @@ class TodosContainer extends Component {
     }
 }
 
-export default TodosContainer;
+export default connect(
+    ({ todo }) => ({
+        input: todo.input,
+        todos: todo.todos
+    }),
+    (dispatch) => ({
+        TodoActions: bindActionCreators(todoActions, dispatch)
+    })
+)(TodosContainer);
